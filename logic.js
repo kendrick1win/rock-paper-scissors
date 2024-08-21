e3ae6ad55d8fc8b89d46243e27b265a99eff0573
Kendrick Lwin <Khant.Lwin.2023@live.rhul.ac.uk>
2024-08-21 09:28:33 +0100

    completed the getComputerChoice for the computer player

--- logic.js
+++ logic.js
@@ -1,1 +1,18 @@
 console.log("Hello World")
+
+function getRandomInt() {
+    return Math.floor(Math.random() * 3);
+  }
+  
+function getComputerChoice(){
+    let result = getRandomInt();
+    if(result == 0){
+        return "Rock"
+    }
+    else if(result == 1){
+        return "Paper"
+    }
+    else if(result == 2){
+        return "Scissors"
+    }
+}
