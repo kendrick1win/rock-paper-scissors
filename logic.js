d5a58147b5bc5e1cd58f44c04926820f99d85871
Kendrick Lwin <Khant.Lwin.2023@live.rhul.ac.uk>
2024-08-21 10:54:43 +0100

    completed the playRound() function to play a round

--- logic.js
+++ logic.js
@@ -6,9 +6,9 @@ function getRandomInt() {
   
 function getComputerChoice(){
     let result = getRandomInt();
-    let rock = "Rock";
-    let paper = "Paper";
-    let scissors = "Scissors";
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
     if(result == 0){
         return rock
     }
@@ -22,9 +22,9 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let humanChoice = prompt("Rock Paper or Scissors?");
-    let rock = "Rock";
-    let paper = "Paper";
-    let scissors = "Scissors";
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
     let errorMessage = "You must enter a valid word";
     if (humanChoice.toLowerCase() == "rock"){
         return rock;
@@ -39,6 +39,57 @@ function getHumanChoice(){
         return errorMessage;
     }
 }
-
 let humanScore = 0;
 let computerScore = 0;
+function playRound(humanChoice, computerChoice){
+    let winningMessage = "You Win!";
+    let losingMessage = "You Lose";
+    let tieMessage = "You tied";
+    let totalScore = humanScore + " " + computerScore;
+    if (humanChoice === computerChoice) {
+        console.log(tieMessage);
+        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+    }
+    if (humanChoice == "rock"){
+        if(computerChoice == "scissors"){
+            console.log(winningMessage);
+            humanScore +=1;
+            console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+        }
+        else{
+            console.log(losingMessage)
+            computerScore += 1;
+            console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+        }
+    }
+    if (humanChoice == "paper"){
+        if(computerChoice == "rock"){
+            console.log(winningMessage);
+            humanScore +=1;
+            console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+        }
+        else{
+            console.log(losingMessage)
+            computerScore += 1;
+            console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+        }
+    }
+    if (humanChoice == "scissors"){
+        if(computerChoice == "paper"){
+            console.log(winningMessage);
+            humanScore +=1;
+            console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+        }
+        else{
+            console.log(losingMessage)
+            computerScore += 1;
+            console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+        }
+    }
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
